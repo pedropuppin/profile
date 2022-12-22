62f94f6d50447f5bc8e2242baa91e46d101bd3ff
pedro puppin <puppin.pedro@gmail.com>
2022-12-22 14:55:01 -0300

    tattoo slider first version/ curved text buttom

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// skills slider
 var swiper = new Swiper('.swiper', {
   slidesPerView: 3,
   spaceBetween: 50,
@@ -39,6 +40,7 @@ function getDirection() {
   return direction;
 }
 
+// navbar onscroll color
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 56) {
     document.querySelector('.navbar').classList.add('navbar-scrolled')
@@ -47,6 +49,7 @@ window.addEventListener('scroll', () => {
   }
 })
 
+// typed.js
 if (window.innerWidth >= 1400) {
   strings = ["Hi there, I'm Pedro!", "Full-Stack Web Dev."]
 } else {
@@ -58,3 +61,62 @@ var typed = new Typed('#typed', {
   typeSpeed: 50,
   loop: true
 });
+
+// btn toggle
+const toggleSlider = () => {
+  document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+}
+
+// curve text
+const text = document.getElementById('curved-text');
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+const ele = text.querySelectorAll('span');
+for(var i = 0; i < ele.length; i++) {
+    ele[i].style.transform = "rotate("+i * 15.5 +"deg)";
+}
+
+// tattoo slider
+const nav = document.getElementById("tattoo-slider"),
+      track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";
+  track.dataset.prevPercentage = track.dataset.percentage; //store % on mouse release
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage, //use the stored % as the new starting point
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+  track.dataset.percentage = nextPercentage; //constantly track %
+
+  track.animate({
+    transform: `translate(${nextPercentage}%, -65%)`
+  }, { duration: 1200, fill: "forwards" });
+
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+nav.onmousedown = e => handleOnDown(e);
+
+nav.ontouchstart = e => handleOnDown(e.touches[0]);
+
+nav.onmouseup = e => handleOnUp(e);
+
+nav.ontouchend = e => handleOnUp(e.touches[0]);
+
+nav.onmousemove = e => handleOnMove(e);
+
+nav.ontouchmove = e => handleOnMove(e.touches[0]);
